fd1d1064e7f29f2f8874d27c7c87c8a582d5ded2
chawalitc <golfcssp2@gmail.com>
2014-06-06 23:11:27 +0700

    refactor convertRoman test3

--- src/main/java/th/golf/kata/RomanNumerals/ConvertRomanNumerals.java
+++ src/main/java/th/golf/kata/RomanNumerals/ConvertRomanNumerals.java
@@ -3,14 +3,12 @@ package th.golf.kata.RomanNumerals;
 public class ConvertRomanNumerals{
 	
 	public String convertRoman(int number){
-		
-		if(number == 3){
-			return "III";
+
+		String romanNum ="";
+		for(int i=0;i<number;i++){
+			romanNum += "I";
 		}
-		if(number == 2){
-			return "II";
-		}
-		return "I";
+		return romanNum;
 	}
 
 }
