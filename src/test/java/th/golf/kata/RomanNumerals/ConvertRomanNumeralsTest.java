5789e3bc5bc3490d6273fdc6ff3b77bb6535fa54
chawalitc <golfcssp2@gmail.com>
2014-06-11 22:29:00 +0700

    maintain testRomanNumber

--- src/test/java/th/golf/kata/RomanNumerals/ConvertRomanNumeralsTest.java
+++ src/test/java/th/golf/kata/RomanNumerals/ConvertRomanNumeralsTest.java
@@ -8,18 +8,8 @@ public class ConvertRomanNumeralsTest{
 	ConvertRomanNumerals romanNumerals = new ConvertRomanNumerals();
 
 	@Test
-	public void testConvert1ReturnI(){	
-		assertEquals("I",romanNumerals.convertRoman(1));
-	}
-
-	@Test
-	public void testConvert2ReturnII(){
+	public void testConvertLessThan4ReturnI(){
 		assertEquals("II",romanNumerals.convertRoman(2));
-	}
-
-	@Test
-	public void testConvert3ReturnIII(){
-		assertEquals("III",romanNumerals.convertRoman(3));
 	}
 
 	@Test
@@ -33,12 +23,8 @@ public class ConvertRomanNumeralsTest{
 	}
 
 	@Test
-	public void testConvert6ReturnVI(){
+	public void testConvertMoreThan5ReturnVI(){
 		assertEquals("VI",romanNumerals.convertRoman(6));
 	}
 
-	@Test
-	public void testConvert7ReturnVII(){
-		assertEquals("VII",romanNumerals.convertRoman(7));
-	}
 }
