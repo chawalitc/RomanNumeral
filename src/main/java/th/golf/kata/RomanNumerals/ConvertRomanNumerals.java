2e574b0e771a7470dafed1aa1e3fd92e0f4c4378
chawalitc <golfcssp2@gmail.com>
2014-06-11 23:32:20 +0700

    Refactor code RomanNumber1-9

--- src/main/java/th/golf/kata/RomanNumerals/ConvertRomanNumerals.java
+++ src/main/java/th/golf/kata/RomanNumerals/ConvertRomanNumerals.java
@@ -6,22 +6,32 @@ public class ConvertRomanNumerals{
 
 		String romanNum ="";
 
-		if(number == 9 ){
-			romanNum = "IX";
-			number = number-9;
-		}
-		if(number >= 5){
-			romanNum += "V";
-			number = number-5;
-		}
-		if(number == 4 ){
-			romanNum = "IV";
-			number = number-4;
-		}
+		romanNum += getRomanNumber(number,9,"IX");
+		number = minusRoman(number,9);
+
+		romanNum += getRomanNumber(number,5,"V");
+		number = minusRoman(number,5);
+
+		romanNum += getRomanNumber(number,4,"IV");
+		number = minusRoman(number,4);
+
 		for(int i=0;i<number;i++){
 			romanNum += "I";			
 		}
 		return romanNum;
 	}
 
+	private int minusRoman (int number,int compareNum) {
+		if(number >= compareNum){
+			number = number-compareNum;
+		}
+		return number;
+	}
+
+	private String getRomanNumber (int number,int compareNum ,String romanNum) {
+		if(number >= compareNum){
+			return romanNum;
+		}
+		return "";
+	}
 }
